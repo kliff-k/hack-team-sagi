3640c2c47e43a34d8c84e2f4f716e41b86ed9c99
Hector Margittay <hectormargittay@live.com>
2019-07-26 02:55:41 -0300

    Alterações no menu lateral e fundo da página

--- js/funcoes.js
+++ js/funcoes.js
@@ -338,3 +338,22 @@ function generateRandomBrowser(){
     return arr[Math.floor(Math.random()*arr.length)];    
 }
 
+
+function initSidenav() {
+    // $("#sidenav").mCustomScrollbar({
+    //     theme: "minimal"
+    // });
+
+    let close_callback = function(){
+        $('#sidenav').removeClass('active');
+        $('#sidenav-overlay').removeClass('active');
+    };
+
+    $('#dismiss, #sidenav-overlay').on('click', close_callback);
+    $(window).resize(close_callback);
+
+    $('#sidenav-toggle').on('click', function(){
+        $('#sidenav').addClass('active');
+        $('#sidenav-overlay').addClass('active');
+    });
+}
